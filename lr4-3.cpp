f4810a1cc6871c2ea407d55378e7bd763997fb63
liana <lianabub07@gmail.com>
2024-10-26 10:09:36 +0300

    Add files via upload

---
+++ lr4-3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int x, a, b, c;
+	for (int i = 100; i < 1000; i++)
+	{
+		x = i;
+		a = x / 100;
+		x -= a * 100;
+		b = x / 10;
+		x -= b * 10;
+		c = x;
+		if (a * c * 2 == b)
+		{
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
